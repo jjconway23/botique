542122fd6893a538a2cf6f1ea725fc872676877e
jjconway23 <jacob.peat01@gmail.com>
2023-02-15 15:07:51 +0000

    create checkout success view and url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import checkout
+from .views import checkout, checkout_success
 
 urlpatterns = [
     path('', checkout, name="checkout"),
+    path('checkout_success.<order_number>', checkout_success, name="checkout_success"),
 ]
