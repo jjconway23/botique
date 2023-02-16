ddcde8267d9abc0acfcaa48df0ecb8ad9738ad0b
jjconway23 <jacob.peat01@gmail.com>
2023-02-16 15:34:41 +0000

    added order history and updated checkout views

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import profile
+from .views import profile, order_history
+
+
 urlpatterns = [
     path('', profile, name="profile"),
+    path('order_history/<order_number>', order_history, name="order_history"),
 
 ]
