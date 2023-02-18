fd72c6389ae6f8ac16062d9d46a9849257164e9c
jjconway23 <jacob.peat01@gmail.com>
2023-02-18 16:34:58 +0000

    Added ability for superusers to delete product

--- products/views.py
+++ products/views.py
@@ -77,9 +77,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -106,7 +106,7 @@ def edit_product(request, product_id):
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
-        
+
     template = 'products/edit_product.html'
     context = {
         'form': form,
@@ -114,3 +114,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Product deleted! ')
+    return redirect(reverse('products'))
